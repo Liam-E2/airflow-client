5b879b0b8e612ecfa324a267f1a445f2e54f6e7b
Liam Earley <liam.earley@definitivehc.com>
2024-10-25 13:11:56 -0400

    Basic config editing

--- src/config.py
+++ src/config.py
@@ -1,9 +1,19 @@
-import os
+import os, platform
 import json
+import subprocess
 
 SRC_DIR = os.path.dirname(__file__)
-
+CONFIG_FILE = os.path.join(SRC_DIR, 'config.json')
 
 def get_config():
-    with open(os.path.join(SRC_DIR, 'config.json'), 'rb') as f:
+    with open(CONFIG_FILE, 'rb') as f:
         return json.load(f)
+
+
+def edit_config(*args, **kwargs):
+    if platform.system() == 'Windows':
+        os.startfile(CONFIG_FILE)
+
+    subprocess.check_call(["open", CONFIG_FILE])
+
+
