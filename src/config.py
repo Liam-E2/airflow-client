c0e27b24bdb44618e51594e50aeba2cb23310679
Liam Earley <liam.earley@definitivehc.com>
2024-11-13 00:25:19 -0500

    default blank config from config schema

--- src/config.py
+++ src/config.py
@@ -1,6 +1,6 @@
 import os, platform, getpass
 import json
-from dataclasses import dataclass
+from dataclasses import dataclass, asdict
 import subprocess
 
 SRC_DIR = os.path.join(os.path.expanduser('~'+getpass.getuser()), 'airflow_pycli')
@@ -9,22 +9,23 @@ CONFIG_FILE = os.path.join(SRC_DIR, 'config.json')
 
 @dataclass
 class ClientConfig:
-    airflow_session_token: str
-    airflow_base_url: str
+    airflow_session_token: str = None
+    airflow_base_url: str = None
 
 
 def get_config():
-    if not os.path.exists(CONFIG_FILE):
-        os.makedirs(SRC_DIR, exist_ok=True)
-        with open(CONFIG_FILE, 'w') as f:
-            json.dump({}, f)
-
     with open(CONFIG_FILE, 'rb') as f:
         return json.load(f)
 
 
 def edit_config(*args, **kwargs):
+    print(CONFIG_FILE)
+    if not os.path.exists(CONFIG_FILE):
+        os.makedirs(SRC_DIR, exist_ok=True)
+        with open(CONFIG_FILE, 'w') as f:
+            json.dump(asdict(ClientConfig()), f, indent=4)
+    
     if platform.system() == 'Windows':
         os.startfile(CONFIG_FILE)
-
-    subprocess.check_call(["open", CONFIG_FILE])
+    else:
+        subprocess.check_call(["open", CONFIG_FILE])
