e407ecc1e43ea0ca5e213cf19e9f557506c0916f
Liam Earley <liam.earley@definitivehc.com>
2024-11-13 00:10:09 -0500

    config schema dataclass

--- src/config.py
+++ src/config.py
@@ -1,9 +1,17 @@
 import os, platform, getpass
 import json
+from dataclasses import dataclass
 import subprocess
 
 SRC_DIR = os.path.join(os.path.expanduser('~'+getpass.getuser()), 'airflow_pycli')
 CONFIG_FILE = os.path.join(SRC_DIR, 'config.json')
+
+
+@dataclass
+class ClientConfig:
+    airflow_session_token: str
+    airflow_base_url: str
+
 
 def get_config():
     if not os.path.exists(CONFIG_FILE):
