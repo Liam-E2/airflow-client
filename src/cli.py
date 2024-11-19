80633429aa22986845bf9f37110f34fc45160da1
Liam Earley <liam.earley@definitivehc.com>
2024-11-18 21:13:50 -0500

    Make dag id a positional argument

--- src/cli.py
+++ src/cli.py
@@ -78,7 +78,7 @@ def dag(subs: argparse._SubParsersAction):
     )
 
     list_dagruns = dag_subs.add_parser("runs", help="List DAG runs given DAG ID")
-    list_dagruns.add_argument("-i", "--dag_id", help="DAG id", type=str)
+    list_dagruns.add_argument("dag_id", help="DAG id", type=str)
     list_dagruns.add_argument("-l", "--limit", type=int, default=10)
     list_dagruns.add_argument("-o", "--offset", type=int, default=0)
     list_dagruns.add_argument("-ob", "--order_by", type=str, help="Field to order by, prefix with - for reverse", default="-start_date")
@@ -111,7 +111,7 @@ def dag(subs: argparse._SubParsersAction):
     list_dagruns.set_defaults(cmd=list_runsfn)
 
     pause_dag = dag_subs.add_parser("pause")
-    pause_dag.add_argument("-i", "--dag_id", help="DAG ID", type=str)
+    pause_dag.add_argument("dag_id", help="DAG ID", type=str)
     pause_dag.add_argument('--unpause', action="store_false")
 
 
